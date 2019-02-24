18be33d4486a1a8570b7ad03438a9a02552d1fac
Andrae Lewis <andrae.lewis@gmail.com>
2019-02-24 01:30:37 -0500

    h

--- main.js
+++ main.js
@@ -1,42 +1,40 @@
-function byID(id) {
-	return document.getElementById(id);
-}
-
-window.onscroll = function () {
-	scrollFunction()
-};
-
-function scrollFunction() {
-	if (window.pageYOffset >= byID("company-description").offsetHeight - 90) {
-		byID("AP").className = "active";
-		byID("AC").className = "";
-		return true;
-	} else {
-		byID("AP").className = "";
-		byID("AC").className = "active";
-		return false;
-	}
-}
-
 // Get the modal
-var imgs = new Array(document.getElementsByClassName('clmimg'));
-imgs.forEach(image => {
-	var modal = document.getElementById('myModal');
-	// Get the image and insert it inside the modal - use its "alt" text as a caption
-	var img = image;
-	var modalImg = document.getElementById("m_img");
-	var captionText = document.getElementById("caption");
-	img.onclick = function () {
-		modal.style.display = "block";
-		modalImg.src = this.src;
-		captionText.innerHTML = this.alt;
-	}
+$(document).ready(function(){
+	$("#wrapper").hide();
+	$("#navbar").hide();
+	$("#footer").hide();
+	$(".loading").ready(function(){
+		$(".loading").delay(2000).fadeOut("slow");
+	});
+	$("#navbar").ready(function(){
+		$("#navbar").delay(2000).slideDown("slow");
+	});
+	$("#wrapper").ready(function(){
+		$("#wrapper").delay(2300).fadeIn("slow");
+	});
+	$("#footer").ready(function(){
+		$("#footer").delay(2300).fadeIn("slow");
+	});
+	
+	var list = document.getElementsByClassName('clmimg');
+	var imgs = [].slice.call(list);
+	runtime = 0;
+	imgs.forEach(image => {
+		var modal = document.getElementById('myModal');
+		// Get the image and insert it inside the modal - use its "alt" text as a caption
+		var img = image;
+		var modalImg = document.getElementById("m_img");
+		var captionText = document.getElementById("caption");
+		img.onclick = function () {
+			modal.style.display = "block";
+			$("#modal").toggleClass("visible");
+			modalImg.src = this.src;
+			captionText.innerHTML = this.alt;
+		}
+		console.log(runtime);
+		runtime += 1;
+	});
 });
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-	modal.style.display = "none";
+var close_modal = function () {
+	$("#myModal").fadeOut("slow");
 }
